d45c1b6de2c84932be0c4f4da475c45ab7476776
dale <hessdale96@gmail.com>
2023-04-15 11:01:33 -0600

    messing around with component life cycles

---
+++ src/assets/LifeCycleExample.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default{
+    
+
+beforeCreate:function (){
+console.log(`before component is mouneted`);
+},
+created:function () {
+    console.log(`component has been created`);
+},
+beforeMount:function () {
+console.log(`before component hase been mounted`);
+},
+mounted:function () {
+    console.log(`component has been mounted`);
+},
+};
+</script>
+
+<style scoped>
+
+</style>
